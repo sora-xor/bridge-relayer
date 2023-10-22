1b5fdd02e74d5d5e3c1392f607e9dc3c3c69cc0f
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2023-10-22 12:21:07 +0300

    ci: fix metadata

--- substrate-gen/build.rs
+++ substrate-gen/build.rs
@@ -34,16 +34,16 @@ use std::path::PathBuf;
 use std::str::FromStr;
 
 fn main() {
-    let out_dir = PathBuf::from_str(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
-        .unwrap()
-        .join("src/bytes");
-
-    let branch = &std::env::var("SUBSTRATE_METADATA_BRANCH").unwrap_or("develop".to_string());
-    let target = format!(
-        "https://github.com/sora-xor/sora2-network/raw/{}/substrate-gen/src/bytes/metadata.scale",
-        branch
-    );
-    let response = reqwest::blocking::get(target).unwrap().bytes().unwrap();
-    std::fs::create_dir_all(&out_dir).unwrap();
-    std::fs::write(out_dir.join("metadata.scale"), response.to_vec()).unwrap();
+    // let out_dir = PathBuf::from_str(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
+    //     .unwrap()
+    //     .join("src/bytes");
+    //
+    // let branch = &std::env::var("SUBSTRATE_METADATA_BRANCH").unwrap_or("develop".to_string());
+    // let target = format!(
+    //     "https://github.com/sora-xor/sora2-network/raw/{}/substrate-gen/src/bytes/metadata.scale",
+    //     branch
+    // );
+    // let response = reqwest::blocking::get(target).unwrap().bytes().unwrap();
+    // std::fs::create_dir_all(&out_dir).unwrap();
+    // std::fs::write(out_dir.join("metadata.scale"), response.to_vec()).unwrap();
 }
