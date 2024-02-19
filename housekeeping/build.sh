f7fb495af2d5827f2350d62b3f20a2707575b3b8
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-02-19 09:29:14 +0300

    update
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/build.sh
+++ housekeeping/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 set -e
-cargo b -r
+build() {
+    cargo b -r
+}
+
+test() {
+    export RUSTFLAGS="-Cinstrument-coverage"
+    export SKIP_WASM_BUILD=1    
+    export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
+    cargo test
+}
--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-export RUSTFLAGS="-Cinstrument-coverage"
-export SKIP_WASM_BUILD=1
-export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
-
-cargo test
-rustup component add llvm-tools-preview
-grcov . --binary-path ./target/debug -s . -t lcov --branch -o ./lcov_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug"
+grcov . --binary-path ./target/debug -s . -t lcov --branch -o ./lcov_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" --log-level="ERROR" --llvm-path='/usr/lib/llvm-14/bin'
 find . -type f -name '*.profraw' -delete
