96317fa8ccd566db0eb9cc814adfe83d602f06ae
Dmitry <bushuevdv@mail.ru>
2024-03-05 12:22:47 -0300

    refresh metadata

--- liberland-gen/src/lib.rs
+++ liberland-gen/src/lib.rs
@@ -46,14 +46,6 @@ pub mod liberland_runtime {
     use crate::UnboundedGenericCommitment;
     #[subxt(substitute_type = "bridge_types::types::GenericCommitmentWithBlock")]
     use crate::UnboundedGenericCommitmentWithBlock;
-    // #[subxt(substitute_type = "bridge_common::beefy_types::BeefyMMRLeaf")]
-    // use ::bridge_common::beefy_types::BeefyMMRLeaf;
-    // #[subxt(substitute_type = "bridge_common::beefy_types::Commitment")]
-    // use ::bridge_common::beefy_types::Commitment;
-    // #[subxt(substitute_type = "bridge_common::beefy_types::ValidatorProof")]
-    // use ::bridge_common::beefy_types::ValidatorProof;
-    // #[subxt(substitute_type = "bridge_common::beefy_types::ValidatorSet")]
-    // use ::bridge_common::beefy_types::ValidatorSet;
     #[subxt(substitute_type = "bridge_common::simplified_proof::Proof")]
     use ::bridge_common::simplified_proof::Proof;
     #[subxt(substitute_type = "bridge_types::ethashproof::DoubleNodeWithMerkleProof")]
@@ -78,14 +70,6 @@ pub mod liberland_runtime {
     use ::bridge_types::HeaderId;
     #[subxt(substitute_type = "bridge_types::SubNetworkId")]
     use ::bridge_types::SubNetworkId;
-    // #[subxt(substitute_type = "sp_beefy::crypto::Public")]
-    // use ::sp_beefy::crypto::Public;
-    // #[subxt(substitute_type = "sp_beefy::mmr::BeefyAuthoritySet")]
-    // use ::sp_beefy::mmr::BeefyAuthoritySet;
-    // #[subxt(substitute_type = "sp_beefy::mmr::MmrLeaf")]
-    // use ::sp_beefy::mmr::MmrLeaf;
-    // #[subxt(substitute_type = "sp_beefy::commitment::Commitment")]
-    // use ::sp_beefy::Commitment;
     #[subxt(substitute_type = "sp_core::ecdsa::Public")]
     use ::sp_core::ecdsa::Public;
     #[subxt(substitute_type = "sp_core::ecdsa::Signature")]
@@ -102,24 +86,15 @@ pub mod liberland_runtime {
     use ::sp_runtime::MultiSignature;
     #[subxt(substitute_type = "sp_runtime::MultiSigner")]
     use ::sp_runtime::MultiSigner;
-    #[subxt(substitute_type = "sp_core::bounded::bounded_btree_map::BoundedBTreeMap")]
+
+    #[subxt(substitute_type = "bounded_collections::bounded_btree_map::BoundedBTreeMap")]
     use ::std::collections::btree_map::BTreeMap;
-    #[subxt(substitute_type = "sp_core::bounded::bounded_btree_set::BoundedBTreeSet")]
+    #[subxt(substitute_type = "bounded_collections::bounded_btree_set::BoundedBTreeSet")]
     use ::std::collections::btree_set::BTreeSet;
-    #[subxt(substitute_type = "sp_core::bounded::bounded_vec::BoundedVec")]
+    #[subxt(substitute_type = "bounded_collections::bounded_vec::BoundedVec")]
     use ::std::vec::Vec;
-    #[subxt(substitute_type = "sp_runtime::bounded::bounded_vec::BoundedVec")]
+    #[subxt(substitute_type = "bounded_collections::bounded_vec::BoundedVec")]
     use ::std::vec::Vec;
-
-    // #[subxt(substitute_type = "sp_core::bounded::bounded_btree_map::BTreeMap")]
-    // use ::std::collections::btree_map::BTreeMap;
-    // #[subxt(substitute_type = "sp_core::bounded::bounded_btree_set::BTreeSet")]
-    // use ::std::collections::btree_set::BTreeSet;
-    // #[subxt(substitute_type = "sp_core::bounded::bounded_vec::Vec")]
-    // use ::std::vec::Vec;
-    // #[subxt(substitute_type = "sp_runtime::bounded::bounded_vec::Vec")]
-    // use ::std::vec::Vec;
-
 }
 
 pub use config::*;
