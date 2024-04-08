b454da8b88183cc4b01a83c9e7d3e59e96119d2e
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-04-08 15:14:43 +0300

    add --future-incompat-report flag
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/build.sh
+++ housekeeping/build.sh
@@ -6,7 +6,7 @@ test() {
     export RUSTFLAGS="-Cinstrument-coverage"
     export SKIP_WASM_BUILD=1    
     export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
-    cargo test --future-incompat-report --timings=fmts
+    cargo test --future-incompat-report --timings
 }
 
 build() {
